2c8701dc09c28e4edb53692525ec407445edda2f
Vlad Moskalchuk <mmvvrr1@gmail.com>
2019-05-04 19:50:06 +0900

    Done!

--- Lab_6/Head.cpp
+++ Lab_6/Head.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 using namespace std;
 
 struct point { int x, y;};
+
 
 int main()
 {
 	setlocale(0, "");
 	int n, i;
 	point *a;
+	point b;
+
 
 	FILE *f;
 
@@ -16,15 +23,35 @@ int main()
 
 	fread(&n, sizeof(int), 1, f);
 
-	cout << n << endl;
+	cout << "В данном файле" << n << " позиций" <<endl;
+	cout << "Введите вашу точку: ";
+	cin >> b.x; cin >> b.y;
 
 	a = new point[n];
+	float *r;
+	r = new float[n];
 
 	fread(a, sizeof(point), n, f);
 
 	for(i = 0; i < n; i++)
 	{
 		cout<< a[i].x << " ; " << a[i].y << endl;
+	}
+
+	for(i = 0; i < n; i++)
+	{
+		cout<< a[i].x << " ; " << a[i].y << endl;
+		r[i] = sqrt(pow((a[i].x-b.x), 2) + pow((a[i].y-b.y), 2));
+	}
+
+	cout << "Вывод равноудаленных точек" << endl;
+
+	for (i = 0; i < n-1; i++)
+	{
+		for (int j = i+1; j < n; j++)
+		{
+			if (r[i] == r[j]) {cout << r[i] << "; " << "(" << a[i].x << ";" << a[i].y << ")" << endl;}
+		}
 	}
 
 	fclose(f);
