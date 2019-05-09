915adac40560b7aea63d41f685e118c2db5594c9
Vlad Moskalchuk <mmvvrr1@gmail.com>
2019-05-09 15:51:23 +0900

    New Class

--- ClassLabs/Lab7.cpp
+++ ClassLabs/Lab7.cpp
@@ -1,1 +1,42 @@
 #include <iostream>
+#include <math.h>
+
+class triangle
+{
+private: 
+	{
+		float a, b, c;
+		float P, S;
+	}
+public:
+	triangle(float na, float nb, float nc)
+	{
+		a = na;
+		b = nb;
+		c = nc;
+
+		P = (1\2)*(a+b+c);
+		S = sqrt(P*(P-a)*(P-b)*(P-c))
+
+		cout << ""
+	}
+
+	triangle(int h)
+	{
+		if (h == 1){
+			cout << "Периметр равен: " << P;
+		}
+		else{
+			cout << "Площадь равна: " << S;
+		}
+	}
+
+	triangle()
+	{
+		cout << "Сторона A = " << a << endle 
+			 << "Сторона B = " << b << endle 
+			 << "Сторона C = " << c << endle 
+			 << "Периметр P = "<< P << endle 
+			 << "Площадь S = " << S << endle;
+	}	
+};
