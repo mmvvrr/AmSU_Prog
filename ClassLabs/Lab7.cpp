7795d87a37ed6c3053dd2313afd7a749542c12dc
Vlad Moskalchuk <mmvvrr1@gmail.com>
2019-05-22 20:14:05 +0900

    Normal Seven Ювелир

--- ClassLabs/Lab7.cpp
+++ ClassLabs/Lab7.cpp
@@ -1,8 +1,10 @@
+#include <stdio.h>
 #include <iostream>
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <clocale>
+#include <windows.h>
 using namespace std;
 
 
@@ -11,7 +13,6 @@ class triangle
 {
 private: 
 		float a, b, c;
-		float P, S;
 public:
 	
 	
@@ -21,8 +22,8 @@ public:
 		b = 4;
 		c = 5;
 	}
-	triangle(float na, float nb, float nc): a(na), b(nb), c(nc){
-	}
+	
+	triangle(float na, float nb, float nc): a(na), b(nb), c(nc){}
 
 	triangle(int h, int s)
 	{
@@ -31,25 +32,89 @@ public:
 		c = rand()%h + s;
 	}
 	
-	void getNaumbers(){
-		cout << "a = " << a << "d = " << b << "c = " << c << endl;
-		cout << "P = " << (a+b+c) << endl;
-		cout << "S = " << sqrt((a+b+c)*(b+c)*(a+c)*(b+c));
-	}
+	float bereich(){
+          return sqrt((a+b+c)*(b+c)*(a+c)*(b+c));
+    }
 	
+	float perimeter(){
+          return (a+b+c);
+    }
+	
+	void getNaumbers(){
+		cout << "a = " << a << endl << "d = " << b << endl << "c = " << c << endl;
+		/*cout << "P = " << (a+b+c) << endl;
+		cout << "S = " << sqrt((a+b+c)*(b+c)*(a+c)*(b+c)) << endl;*/
+	}
+    
+    float getA()
+    {return a;}
+    float getB()
+    {return b;}
+    float getC()
+    {return c;}
+};
+
+class pyramid : public triangle
+{
+      private:
+              float h;
+      public:
+             pyramid(float a, float b, float c, float h): triangle(a,b, c), h(h){}
+             
+             float Volume(){
+                   return 1.0/3.0*(bereich()*h);
+             }
+             
+             float squareSide(){
+                   float f;
+                   f = sqrt(pow(h,2)+pow(getA()/(2*tan(90)),2));
+                   return 1.0/2*perimeter()*f;                   
+             }
+      
 };
 
 int main()
 {
 	srand(time(0));
-	
-	setlocale(0, "");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	float da, db, dc;
-	cin >> da;
-	cin >> db;
-	cin >> dc;
+	int h, s, ch;
+	cout << "Выберете способ ввода(1 - ручной, 2 - рандом, 3 - стандартный): ";
+	cin >> ch;
+	switch(ch)
+	{
+              case 1:
+                   {cout << "Введите стороны треугольника"<< endl;
+                   cout << "Введите a: ";
+                   cin >> da;
+                   cout << "Введите b: ";
+	               cin >> db;
+	               cout << "Введите c: ";
+	               cin >> dc;
+	               triangle obj(da, db, dc);
+               	   obj.getNaumbers();
+               	   cout << "P = " << obj.perimeter() << endl;
+       	   	       cout << "S = " << obj.bereich() << endl;
+	               break;}
+              case 2:
+                   {cout << "Введите пределы рандома для сторон треугольника(от/до)"<< endl;
+                   cin >> h;
+	               cin >> s;
+	               triangle obj(s, h);
+               	   obj.getNaumbers();
+               	   cout << "P = " << obj.perimeter() << endl;
+       	   	       cout << "S = " << obj.bereich() << endl;
+                   break;}
+              case 3:
+                   {cout << "Создание египетского треугольника"<< endl;
+                   triangle obj;
+             	   obj.getNaumbers();
+         	   	   cout << "P = " << obj.perimeter() << endl;
+       	   	       cout << "S = " << obj.bereich() << endl;
+                   break;}
+    }
 	
-	triangle obj;
-	
-	obj.getNaumbers();
+   system("pause")
+   ;
 }
